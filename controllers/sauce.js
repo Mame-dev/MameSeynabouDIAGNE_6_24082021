ed2aa0769c8e02e397ebc17cd0159bd417c07f7f
Mame-dev <mameseynaboudiagne@yahoo.fr>
2021-10-12 11:01:40 +0200

    Démarrer le middleware

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,2 +1,22 @@
 // Récupération du modèle sauce
-const sauce = require('../models/sauce');
+const Sauce = require('../models/sauce');
+
+exports.createSauce = (req, res, next) => {
+
+};
+
+exports.getOneThing = (req, res, next) => {
+
+};
+
+exports.modifyThing = (req, res, next) => {
+
+};
+
+exports.deleteThing = (req, res, next) => {
+
+};
+
+exports.getAllStuff = (req, res, next) => {
+
+};
--- routes/sauce.js
+++ routes/sauce.js
@@ -1,11 +1,21 @@
-/*Import de Express*/
+/*Import de Express pour le router*/
 const express = require('express');
 
 // Appel du router avec la méthode mise à disposition par Express
 const router = express.Router();
 
 // On associe les fonctions aux différentes routes, on importe le controller
-const saucesCtrl = require('../controllers/sauce');
+const sauceCtrl = require('../controllers/sauce');
+
+router.post('/', sauceCtrl.createSauce);
+
+router.put('/:id', );
+
+router.delete('/:id', );
+
+router.get('/:id', );
+
+router.get('/');
 
 // Exportation du modèle userSchema
 module.exports = router;
